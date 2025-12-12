04874e739d7e7e27e9b4c4e0b0f2bb68a896983a
raihanm26 <156015496+raihanm26@users.noreply.github.com>
2025-12-12 00:44:45 -0500

    Use route constants in main

--- investtrack-frontend/src/main.jsx
+++ investtrack-frontend/src/main.jsx
@@ -1,8 +1,14 @@
+/**
+ * File: main.jsx
+ * Purpose: Application entry point; defines routes and renders layout.
+ */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import NavBar from "./components/NavBar.jsx";
+import { ROUTES } from "./constants";
 
 function Proposal() {
   return <h2>Proposal Page (Coming Soon)</h2>;
@@ -16,9 +22,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <NavBar />
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/proposal" element={<Proposal />} />
-      <Route path="/annotated" element={<AnnotatedBibliography />} />
+      <Route path={ROUTES.HOME} element={<App />} />
+      <Route path={ROUTES.PROPOSAL} element={<Proposal />} />
+      <Route path={ROUTES.ANNOTATED} element={<AnnotatedBibliography />} />
     </Routes>
   </BrowserRouter>
 );
