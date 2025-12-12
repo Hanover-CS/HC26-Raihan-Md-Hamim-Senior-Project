a1f7355d52393bdbe7f11ad146659e420ca2c6ac
raihanm26 <156015496+raihanm26@users.noreply.github.com>
2025-12-12 13:10:21 -0500

    Refactor main.jsx to simplify rendering

--- investtrack-frontend/src/main.jsx
+++ investtrack-frontend/src/main.jsx
@@ -1,36 +1,11 @@
-/**
- * File: main.jsx
- * Purpose: Application entry point and route configuration.
- */
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
-import NavBar from "./components/NavBar";
 import App from "./App";
-import Proposal from "./pages/Proposal";
-import AnnotatedBibliography from "./pages/AnnotatedBibliography";
-import { ROUTES } from "./constants";
-
-function Shell() {
-  return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path={ROUTES.HOME} element={<App />} />
-        <Route path={ROUTES.PROPOSAL} element={<Proposal />} />
-        <Route path={ROUTES.ANNOTATED} element={<AnnotatedBibliography />} />
-      </Routes>
-    </>
-  );
-}
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Shell />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
