b9c8309c4be40a251e3327f681a5284e8d6a37a4
raihanm26 <156015496+raihanm26@users.noreply.github.com>
2025-12-12 12:38:17 -0500

    Update main.jsx

--- investtrack-frontend/src/main.jsx
+++ investtrack-frontend/src/main.jsx
@@ -1,30 +1,36 @@
 /**
  * File: main.jsx
- * Purpose: Application entry point; defines routes and renders layout.
+ * Purpose: Application entry point and route configuration.
  */
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import NavBar from "./components/NavBar.jsx";
+import "./index.css";
+
+import NavBar from "./components/NavBar";
+import App from "./App";
+import Proposal from "./pages/Proposal";
+import AnnotatedBibliography from "./pages/AnnotatedBibliography";
 import { ROUTES } from "./constants";
 
-function Proposal() {
-  return <h2>Proposal Page (Coming Soon)</h2>;
-}
-
-function AnnotatedBibliography() {
-  return <h2>Annotated Bibliography (Coming Soon)</h2>;
+function Shell() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path={ROUTES.HOME} element={<App />} />
+        <Route path={ROUTES.PROPOSAL} element={<Proposal />} />
+        <Route path={ROUTES.ANNOTATED} element={<AnnotatedBibliography />} />
+      </Routes>
+    </>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <NavBar />
-    <Routes>
-      <Route path={ROUTES.HOME} element={<App />} />
-      <Route path={ROUTES.PROPOSAL} element={<Proposal />} />
-      <Route path={ROUTES.ANNOTATED} element={<AnnotatedBibliography />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Shell />
+    </BrowserRouter>
+  </React.StrictMode>
 );
