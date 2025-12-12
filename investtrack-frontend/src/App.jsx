c2d48b4843dcf9a98763fb7557f3f54b135a76b5
raihanm26 <156015496+raihanm26@users.noreply.github.com>
2025-12-12 14:55:37 -0500

    Update App.jsx

--- investtrack-frontend/src/App.jsx
+++ investtrack-frontend/src/App.jsx
@@ -1,23 +1,23 @@
-import {
-  filterHoldings,
-  sortHoldings,
-  loadFavorites,
-  saveFavorites,
-} from "./utils/holdings";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+import Home from "./pages/Home";
+import Proposal from "./pages/Proposal";
+import AnnotatedBibliography from "./pages/AnnotatedBibliography";
 
 export default function App() {
   return (
-    <div
-      style={{
-        padding: 40,
-        background: "yellow",
-        color: "black",
-        fontSize: 28,
-        border: "10px solid red",
-      }}
-    >
-      APP IS RENDERING ✅
-    </div>
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/proposal" element={<Proposal />} />
+        <Route
+          path="/annotated-bibliography"
+          element={<AnnotatedBibliography />}
+        />
+      </Routes>
+    </>
   );
 }
-
