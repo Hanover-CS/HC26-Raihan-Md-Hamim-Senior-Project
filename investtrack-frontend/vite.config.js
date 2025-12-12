22e09ad0f6edd306e563494728c5356924c85fb5
raihanm26 <156015496+raihanm26@users.noreply.github.com>
2025-12-12 15:43:38 -0500

    Update vite.config.js

--- investtrack-frontend/vite.config.js
+++ investtrack-frontend/vite.config.js
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: "./src/test-setup.js",
-    globals: true,
-  },
-  server: {
-    hmr: false,
-  },
 });
